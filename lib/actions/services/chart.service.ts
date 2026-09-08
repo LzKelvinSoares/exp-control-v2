5f616f3fb101a32c13e20897e1555e71e93e559a
Kelvin Luz <kelvinsoaresklv@gmail.com>
2026-09-08 19:15:09 +0100

    - Fix build

--- lib/actions/services/chart.service.ts
+++ lib/actions/services/chart.service.ts
@@ -1,12 +1,12 @@
 import { MONTHS } from '@/constants';
 import { Budget, Expense, Fuel, MonthlyChartData } from '@/types/app-types';
-import { AuthContext, IFullTableCrudRepository, IMCPQueryRepository, IReadService } from '@/types/server-types';
+import { AuthContext, IFullTableCrudRepository, IFullMCPQueryRepository, IReadService } from '@/types/server-types';
 import { NextRequest } from 'next/server';
 
 export class ChartService implements IReadService<MonthlyChartData> {
     constructor(
-        private expensesRepository: IMCPQueryRepository<Expense>, 
-        private revenuesRepository: IMCPQueryRepository<Budget>, 
+        private expensesRepository: IFullMCPQueryRepository<Expense>, 
+        private revenuesRepository: IFullMCPQueryRepository<Budget>, 
         private fuelRepository: IFullTableCrudRepository<Fuel>) {
     }
 
